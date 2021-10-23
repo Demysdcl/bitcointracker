f72064b64e3c95e588c2fd5a8a5a059056e05123
Demys Cota <demysdcl@gmail.com>
2021-10-23 11:21:45 -0300

    chore: adding HttpModule and recommendation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -5,7 +6,7 @@ import { BitcoinModule } from './modules/bitcoin/bitcoin.module'
 import { DatabaseModule } from './modules/database/database.module'
 
 @Module({
-  imports: [BitcoinModule, DatabaseModule],
+  imports: [BitcoinModule, DatabaseModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
